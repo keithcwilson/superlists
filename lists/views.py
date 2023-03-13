4d0602784c597343da67911f5a97212f8b4dea0b
keithcwilson <keithcwilson@gmail.com>
2023-03-12 20:27:15 -0400

    Adjust model so that items are associated with different lists.

--- lists/views.py
+++ lists/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import redirect, render
-from .models import Item
+from .models import Item, List
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-
-    items = Item.objects.all()
     return render(request, 'lists/home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'lists/list.html', {'items': items})
+
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
