98cd6b71ec6d0cd0496a19a39202cf0b5221a77d
keithcwilson <keithcwilson@gmail.com>
2023-03-14 18:57:09 -0400

    fix settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -125,3 +125,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['http://*.keithcwilson.com','http://*.127.0.0.1']
