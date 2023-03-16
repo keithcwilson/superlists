a5147b60aadd5ee1434a3661b82ca104938ca970
keithcwilson <keithcwilson@gmail.com>
2023-03-16 18:52:33 -0400

    fix fabric script

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,5 +1,5 @@
 import random
-from fabric.operations import append, exists
+from fabric.operations import
 from fabric.api import cd, env, local, run
 
 REPO_URL = 'https://github.com/keithcwilson/superlists'
@@ -17,7 +17,7 @@ def deploy():
 
 
 def _get_latest_source():
-    if exists('.git'):
+    if run('.git').ok:
         run('git fetch')
     else:
         run(f'get clone {REPO_URL} .')
@@ -26,18 +26,18 @@ def _get_latest_source():
 
 
 def _update_virtualenv():
-    if not exists('virtualenv/bin/pip'):
+    if not run('virtualenv/bin/pip').ok:
         run(f'python3.9 -m venv virtualenv')
     run('./virtualenv/bin/pip install -r requirements.txt')
 
 
 def _create_or_update_dotenv():
-    append('.env', 'DJANGO_DEBUG_FALSE=y')
-    append('.env', f'SITENAME={env.host}')
+    run('echo "DJANGO_DEBUG_FALSE=y" >> .env')
+    run(f'echo "SITENAME={env.host}" >> .env')
     current_contents = run('cat .env')
     if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices('abcdefghijklmnopqrstuvwxyz0123456789', k=50))
-        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+        run(f'echo "DJANGO_SECRET_KEY={new_secret}" >> .env')
 
 
 def _update_static_files():
