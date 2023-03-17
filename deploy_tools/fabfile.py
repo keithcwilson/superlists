c31f1242615afbc4b3b9324e6914a6b90f5a38fd
keithcwilson <keithcwilson@gmail.com>
2023-03-17 10:41:22 -0400

    fix fabric script

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,13 +1,15 @@
 import random
-from fabric.api import cd, env, local, run
+from pathlib import Path
+from fabric import Connection, task
 
 REPO_URL = 'https://github.com/keithcwilson/superlists'
 
 
+@task
 def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.host}'
-    run(f'mkdir -p {site_folder}')
-    with cd(site_folder):
+    site_folder = f'/home/{c.user}/sites/{c.host}'
+    c.run(f'mkdir -p {site_folder}')
+    with c.cd(site_folder):
         _get_latest_source()
         _update_virtualenv()
         _create_or_update_dotenv()
@@ -16,32 +18,32 @@ def deploy():
 
 
 def _get_latest_source():
-    if run('.git').ok:
-        run('git fetch')
+    if Path('.git').exists():
+        c.run('git fetch')
     else:
-        run(f'get clone {REPO_URL} .')
-        current_commit = local("git log -n 1 --format=%H", capture=True)
-        run(f'git reset --hard {current_commit}')
+        c.run(f'get clone {REPO_URL} .')
+    current_commit = c.local("git log -n 1 --format=%H", capture=True)
+    c.run(f'git reset --hard {current_commit}')
 
 
 def _update_virtualenv():
-    if not run('virtualenv/bin/pip').ok:
-        run(f'python3.9 -m venv virtualenv')
-    run('./virtualenv/bin/pip install -r requirements.txt')
+    if not PATH('virtualenv/bin/pip').exists:
+        c.run(f'python3.9 -m venv virtualenv')
+    c.run('./virtualenv/bin/pip install -r requirements.txt')
 
 
 def _create_or_update_dotenv():
-    run('echo "DJANGO_DEBUG_FALSE=y" >> .env')
-    run(f'echo "SITENAME={env.host}" >> .env')
+    c.run('echo "DJANGO_DEBUG_FALSE=y" >> .env')
+    c.run(f'echo "SITENAME={c.host}" >> .env')
     current_contents = run('cat .env')
     if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices('abcdefghijklmnopqrstuvwxyz0123456789', k=50))
-        run(f'echo "DJANGO_SECRET_KEY={new_secret}" >> .env')
+        c.run(f'echo "DJANGO_SECRET_KEY={new_secret}" >> .env')
 
 
 def _update_static_files():
-    run('./virtualenv/bin/python manage.py collectstatic --noinput')
+    c.run('./virtualenv/bin/python manage.py collectstatic --noinput')
 
 
 def _update_database():
-    run('./virtualenv/bin/python manage.py migrate --noinput')
+    c.run('./virtualenv/bin/python manage.py migrate --noinput')
